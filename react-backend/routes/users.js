6e64f20658c5f100bd5d602d764f258f9eaa78e0
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-04-03 00:11:17 -0700

    Making call to raspi API

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,17 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+const http = require('http')
+const pi_options = {
+  hostname: "10.0.0.105",
+  port: 3002,
+  path: '/data',
+  method: 'GET'
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
+  http.get('http://10.0.0.105:3002/data', (apiRes) => {
+    let rawDataBuffer = '';
 
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+    /* A chunk of data has been received from API. */
+    apiRes.on('data', (chunk) => {
+      rawDataBuffer += chunk;
+    });
+
+    /* Finished getting data from API */
+    apiRes.on('end', () => {
+      let jsonData = JSON.parse(rawDataBuffer);
+      let temp = jsonData.temp
+      res.json([{
+        id: 1,
+        username: temp
+      }, {
+        id: 2,
+        username: "D0loresH4ze"
+      }]);
+    });
+  }).on("error", (err) => {
+    console.log(err)
+  }); 
 });
 
 module.exports = router;

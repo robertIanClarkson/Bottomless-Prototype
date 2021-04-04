835094a9346c125006642ff55cffae3d7cad9500
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-04-03 18:28:16 -0700

    Added get rout for temp

--- react-backend/routes/temp.js
+++ react-backend/routes/temp.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 const http = require('http')
 
-/* GET users listing. */
+let data = {
+  temp: 0,
+  time: 0
+};
+
 router.post('/', function(req, res, next) {
   data = req.body
   console.log(data)
   res.sendStatus(200)
+});
+
+router.get('/', function(req, res, next) {
+  res.send(data) 
 });
 
 module.exports = router;

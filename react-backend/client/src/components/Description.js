4161697b7c0d51ef487dcf67bcec60ef46c9a0ab
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-04-06 11:21:27 -0700

    Updated Description

--- react-backend/client/src/components/Description.js
+++ react-backend/client/src/components/Description.js
@@ -11,10 +11,10 @@ export const Description = (props) => {
             <h2>This prototype uses:</h2>
             <ul>
               <li>AWS EC2 for website deployment</li>
-              <li>AWS RDS for database deployment</li>
               <li>ExpressJS for backend</li>
               <li>Reactjs for frontend</li>
-              <li>Bootstrap for styling</li>
+              <li>Bootstrap for CSS styling</li>
+              <li>Google Fonts</li>
               <li>MongoDB for database</li>
               <li>Raspberry Pi to interface with our sensor board</li>
             </ul>
@@ -27,17 +27,19 @@ export const Description = (props) => {
             <h2>How it works?</h2>
             <ul>
               <li>Raspberry Pi reads sensor data off the sensor-board.</li>
-              <li>Pi averages several iterations of data to increase its accuracy and precision.</li>
-              <li>Pi uses TCP to send the averaged sensor data from my IP to the ExpressJS backend located on AWS servers.</li>
-              <li>ExpressJS catches the sent data and forwards the data to the clients React frontend</li>
-              <li>The client (you) clicks "Start Logging" button</li>
-              <li>React sends request to Express to begin logging the sensor data</li>
-              <li>Express now send the data to mongoDB and the user's react frontend</li>
-              <li>data is caught by MongoDB server and stored</li>
-              <li>The client (you) clicks "Stop Logging" button</li>
-              <li>React sends request to Express to stop logging the sensor data and return with results</li>
+              <li>Pi uses TCP to send the sensor data from my IP to the ExpressJS backend located on AWS servers (San Jose).</li>
+              <li>ExpressJS catches the sent data and forwards the data to the client's(your) React frontend</li>
+              <li>The client(you) clicks "Collecting" toggle button</li>
+              <li>React sends request to ExpressJS to begin logging the sensor data</li>
+              <li>ExpressJS now sends the data to both mongoDB and the client's(your) React frontend</li>
+              <li>Data sent to the MongoDB server is stored in a NoSQL document for the user</li>
+              <li>The client(you) clicks "Collecting" toggle button</li>
+              <li>React sends request to Express to stop logging the sensor data</li>
               <li>Express catches message, stops sending data to MongoDB, but keeps sending data to react frontend</li>
+              <li>The client (you) clicks "query"</li>
+              <li>React send a request to ExpressJS to query all the data for the user</li>
               <li>Express preforms query of mongoDB for all the sensor data read and then sends said data to react frontend</li>
+              <li>Queried data is available to the user in both its raw table readings and an interactive line chart</li>
             </ul>
           </div>
         </div>
